990e45852ea829f9d7448b866510ba619f8000ed
Borys Krisko <bvkr@me.com>
2019-07-24 11:40:44 +0300

    Almost finished. Work with constraints and design.

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,17 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var redLight: UIView!
     
+    @IBOutlet weak var redLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
     @IBOutlet weak var greenLight: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var redlightIsOn = false
+    var yellowLightisOn = false
+    var greenLightIsOn = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
         redLight.backgroundColor = UIColor.red
+        yellowLight.backgroundColor = UIColor.yellow
+        greenLight.backgroundColor = UIColor.green
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
+        
+        redLight.layer.cornerRadius = 75
+        yellowLight.layer.cornerRadius = 75
+        greenLight.layer.cornerRadius = 75
     }
-
-
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if  redlightIsOn == false && yellowLightisOn == false && greenLightIsOn == false {
+            redLight.alpha = 1
+            redlightIsOn = true
+            startButton.setTitle("NEXT", for: .normal)
+        } else {
+            if redlightIsOn == true {
+                yellowLight.alpha = 1
+                redLight.alpha = 0.3
+                redlightIsOn = false
+                yellowLightisOn = true
+            } else if yellowLightisOn == true {
+                greenLight.alpha = 1
+                greenLightIsOn = true
+                yellowLight.alpha = 0.3
+                yellowLightisOn = false
+            } else if greenLightIsOn == true {
+                redLight.alpha = 1
+                redlightIsOn = true
+                greenLight.alpha = 0.3
+                greenLightIsOn = false
+            }
+        }
+    }
 }
 
